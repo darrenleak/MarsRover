f77e1c52ecac07141eb8c4d4ffc31d4928366bdd
Darren <dleak22@gmail.com>
2022-10-01 08:31:15 +0200

    Add nvmrc and update index

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const inputData = fs.readFileSync(inputArgs[0], 'utf-8');
 const app = new App()
 const output = app.start(inputData)
 
-console.log(inputData)
-
-console.log(output)
-
-// for (const outputValue of output) {
-//   console.log(outputValue)
-// }
+for (const outputValue of output) {
+  console.log(outputValue)
+}
